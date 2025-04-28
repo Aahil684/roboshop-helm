37e604bed7621a39875192ff8eedee8b9dc28490
uzmaa <uzmaashfaq.shavsi@gmail.com>
2025-04-28 20:20:48 +0530

    This is my first terraform file.

--- argocd.sh
+++ argocd.sh
@@ -1,14 +1,13 @@
 URL=$(kubectl get svc argocd-server -n argocd | grep argocd| awk '{print $4}')
-PASSWORD=$(kubectl get secrets argocd-initial-admin-secret -n argocd -o json | jq .data.password|xargs | base64 --decode)
+PASSWORD=$(kubectl get secrets -n argocd argocd-initial-admin-secret -o json | jq .data.password|xargs | base64 --decode)
 argocd login $URL --grpc-web --insecure --username admin --password $PASSWORD
 
 component_name=$1
 env=$2
 imageTag=$3
 
-
-
-
-
 argocd app create $(component_name)  --upsert --repo https://github.com/Aahil684/roboshop-helm --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values  env-$(env)/$(component_name).yaml --helm-set imageTag=$(imageTag)
 argocd app sync $(component_name)
+
+
+
